b646836697a4426828fcca837a8f050b421b4e54
roncan <65841016+roncan1@users.noreply.github.com>
2021-05-26 00:43:23 +0900

    day3_1 (기타 가테고리 추가)

--- app/src/main/java/com/exemple/Launchpad/MainActivity.java
+++ app/src/main/java/com/exemple/Launchpad/MainActivity.java
@@ -37,22 +37,19 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 select = "drum";
                 cate_set(select);
-            }
-        });
+            }});
         button_piano.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 select = "piano";
                 cate_set(select);
-            }
-        });
+            }});
         button_guitar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 select = "guitar";
                 cate_set(select);
-            }
-        });
+            }});
 
         button_1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -102,6 +99,30 @@ public class MainActivity extends AppCompatActivity {
                 soundPool.play(sound8,1f,1f,0,0,1f);
             }
         });
+        button_9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                soundPool.play(sound9,1f,1f,0,0,1f);
+            }
+        });
+        button_10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                soundPool.play(sound10,1f,1f,0,0,1f);
+            }
+        });
+        button_11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                soundPool.play(sound11,1f,1f,0,0,1f);
+            }
+        });
+        button_12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                soundPool.play(sound12,1f,1f,0,0,1f);
+            }
+        });
 
 
     }
@@ -117,7 +138,21 @@ public class MainActivity extends AppCompatActivity {
                 sound6 = soundPool.load(this, R.raw.drum_snare2, 0);
                 sound7 = soundPool.load(this, R.raw.drum_stick, 0);
                 sound8 = soundPool.load(this, R.raw.drum_tom, 0);
-
+                break;
+            case "guitar":
+                sound1 = soundPool.load(this, R.raw.guitar_ab, 0);
+                sound2 = soundPool.load(this, R.raw.guitar_b, 0);
+                sound3 = soundPool.load(this, R.raw.guitar_bb, 0);
+                sound4 = soundPool.load(this, R.raw.guitar_c1, 0);
+                sound5 = soundPool.load(this, R.raw.guitar_c2, 0);
+                sound6 = soundPool.load(this, R.raw.guitar_d, 0);
+                sound7 = soundPool.load(this, R.raw.guitar_e1, 0);
+                sound8 = soundPool.load(this, R.raw.guitar_e2, 0);
+                sound9 = soundPool.load(this, R.raw.guitar_eb, 0);
+                sound10 = soundPool.load(this, R.raw.guitar_f1, 0);
+                sound11 = soundPool.load(this, R.raw.guitar_f2, 0);
+                sound12 = soundPool.load(this, R.raw.guitar_g, 0);
+                break;
 
         }
     }
